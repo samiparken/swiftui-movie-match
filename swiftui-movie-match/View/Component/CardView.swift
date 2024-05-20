dca1f0f85dcf57d21cb7696a5cc4df1f6d6d99eb
samiparken <samiparken@gmail.com>
2024-05-20 08:56:10 +0200

    Movie: add default values

--- swiftui-movie-match/View/Component/CardView.swift
+++ swiftui-movie-match/View/Component/CardView.swift
@@ -1,75 +1,67 @@
 import SwiftUI
 
 struct CardView: View, Identifiable {
-    let id = UUID() //for Identifiable
-    var movie: Movie
-    
-    var body: some View {
-        AsyncImage(
-            url: URL(string: movie.posterPath?.toImageUrl() ?? "")) { phase in
-            switch phase {
-            case .empty:
-                // Placeholder view while loading
-                Text("empty")
-            case .success(let image):
-                // Success: Show the image
-                image
-                    .resizable()
-                    .cornerRadius(24)
-                    .scaledToFit()
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .overlay(
-                        VStack(alignment: .center, spacing: 12) {
-                            Text(movie.title.uppercased()) //title
-                                .foregroundColor(Color.white)
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .shadow(radius: 1)
-                                .padding(.horizontal, 18)
-                                .padding(.vertical, 4)
-                                .overlay(
-                                    Rectangle()
-                                        .fill(Color.white)
-                                        .frame(height: 1),
-                                    alignment: .bottom
-                                )
-                            Text(movie.overview) // overview
-                                .foregroundColor(Color.black)
-                                .font(.footnote)
-                                .fontWeight(.bold)
-                                .frame(minWidth: 85)
-                                .padding(.horizontal, 10)
-                                .padding(.vertical, 5)
-                                .background(
-                                    Capsule().fill(Color.white)
-                                )
-                        }
-                        .frame(minWidth: 280) //VStack frame
-                        .padding(.bottom, 50), //VStack padding
-                        alignment: .bottom //VStack alignment
-                    )
-            case .failure:
-                // Error: Show placeholder or error message
-                Text("Failed to load image")
-            @unknown default:
-                // Placeholder view while loading
-                Text("Placeholder")
-            }
+  let id = UUID() //for Identifiable
+  var movie: Movie = Movie()
+  
+  var body: some View {
+    AsyncImage(
+      url: URL(string: movie.posterPath!.toImageUrl())) { phase in
+        switch phase {
+        case .empty:
+          // Placeholder view while loading
+          Text("empty")
+        case .success(let image):
+          // Success: Show the image
+          image
+            .resizable()
+            .cornerRadius(24)
+            .scaledToFit()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .overlay(
+              VStack(alignment: .center, spacing: 12) {
+                Text(movie.title.uppercased()) //title
+                  .foregroundColor(Color.white)
+                  .font(.largeTitle)
+                  .fontWeight(.bold)
+                  .shadow(radius: 1)
+                  .padding(.horizontal, 18)
+                  .padding(.vertical, 4)
+                  .overlay(
+                    Rectangle()
+                      .fill(Color.white)
+                      .frame(height: 1),
+                    alignment: .bottom
+                  )
+                Text(movie.overview) // overview
+                  .foregroundColor(Color.black)
+                  .font(.footnote)
+                  .fontWeight(.bold)
+                  .frame(minWidth: 85)
+                  .padding(.horizontal, 10)
+                  .padding(.vertical, 5)
+                  .background(
+                    Capsule().fill(Color.white)
+                  )
+              }
+                .frame(minWidth: 280) //VStack frame
+                .padding(.bottom, 50), //VStack padding
+              alignment: .bottom //VStack alignment
+            )
+        case .failure:
+          // Error: Show placeholder or error message
+          Text("Failed to load image")
+        @unknown default:
+          // Placeholder view while loading
+          Text("Placeholder")
         }
-    }
+      }
+  }
 }
 
 struct CardView_Previews: PreviewProvider{
   static var previews: some View {
-      let movieManager = MovieManager() // Create an instance of MovieManager
-      
-      if let firstMovie = movieManager.movieList?.first {
-          // Safely unwrap the first movie if movieList is not nil
-          CardView(movie: firstMovie)
-              .previewLayout(.fixed(width: 375, height: 600))
-      } else {
-          // Handle the case where movieList is nil or empty
-          Text("No movies available")
-      }
-  }}
-
+    CardView(movie: Movie())
+      .previewLayout(.fixed(width: 375, height: 600))
+  }
+}
