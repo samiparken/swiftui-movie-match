a54bbf4c9f36552820250139d02e65ff355a0a6d
samiparken <samiparken@gmail.com>
2024-05-23 15:11:56 +0200

    Add widget target

---
+++ Widgets/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Widgets
+//
+//  Created by Han-Saem Park on 2024-05-23.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by Han-Saem Park on 2024-05-23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsLiveActivity()
+    }
+}
