6b4ec835772cc0771af7117d549e3d9b2f4fa05e
samiparken <samiparken@gmail.com>
2024-05-29 13:41:06 +0200

    APIs: minor concurrency change

--- swiftui-movie-match/API Service/APIs.swift
+++ swiftui-movie-match/API Service/APIs.swift
@@ -4,12 +4,12 @@ import Foundation
 
 func APIgetPopularMovieList(page: Int, language: String) async throws -> MovieResponse {
   let e = EndpointCase.movieListPopular(language: language, page: page, region: "Sweden")
-  let result = try await makeAPIRequest(endpoint: e, responseType: MovieResponse.self)
-  return result
+  async let result = makeAPIRequest(endpoint: e, responseType: MovieResponse.self)
+  return try await result
 }
 
 func APIgetMovieDetail(id: Int, language: String) async throws -> MovieDetail {
   let e = EndpointCase.movieDetail(id: id, language: language)
-  let result = try await makeAPIRequest(endpoint: e, responseType: MovieDetail.self)
-  return result
+  async let result = makeAPIRequest(endpoint: e, responseType: MovieDetail.self)
+  return try await result
 }
