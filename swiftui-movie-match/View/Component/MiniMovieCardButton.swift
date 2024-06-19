9aa4b61d6852158a57e170902197fbdf60e611cd
samiparken <samiparken@gmail.com>
2024-06-19 12:36:58 +0200

    minor change

--- swiftui-movie-match/View/Component/MiniMovieCardButton.swift
+++ swiftui-movie-match/View/Component/MiniMovieCardButton.swift
@@ -4,22 +4,45 @@ import Observation
 import ComposableArchitecture
 
 struct MiniMovieCardButton: View {
-  @Environment(\.colorScheme) var colorScheme
   
+  //MARK: - PROPERTIES
+  @Environment(\.colorScheme) var colorScheme
   @Binding var navStack: [NavRoute]
   @State private var uiImage: UIImage? = nil
   @State private var isLoading = true
-
   let id = UUID() //for Identifiable
   var movie: FavoriteMovie
-  
   var movieManager = MovieManager()
 
   //MARK: - INIT
-  init(navStack: Binding<[NavRoute]>, movie: FavoriteMovie, movieManager: MovieManager) {
+  init(navStack: Binding<[NavRoute]>, 
+       movie: FavoriteMovie,
+       movieManager: MovieManager) {
     self._navStack = navStack
     self.movie = movie
     self.movieManager = movieManager
+  }
+  
+  //MARK: - METHOD
+  private func loadImage(from url: String) {
+    guard let imageUrl = URL(string: url) else {
+      isLoading = false
+      return
+    }
+    
+    Task { //DispatchQueue.global().async
+      if let data = try? Data(contentsOf: imageUrl),
+         let image = UIImage(data: data) {
+        DispatchQueue.main.async {
+          self.uiImage = image
+          self.isLoading = false
+        }
+      } else {
+        DispatchQueue.main.async {
+          self.isLoading = false
+        }
+      }
+    }
   }
   
   //MARK: - BODY
@@ -45,6 +68,7 @@ struct MiniMovieCardButton: View {
         }
       }
       
+      // Movie Title
       Text(movie.title)
         .foregroundColor(colorScheme == .dark
                          ? .tertiaryColor
@@ -61,30 +85,10 @@ struct MiniMovieCardButton: View {
     }
   }
   
-  private func loadImage(from url: String) {
-    guard let imageUrl = URL(string: url) else {
-      isLoading = false
-      return
-    }
-    
-    Task { //DispatchQueue.global().async
-      if let data = try? Data(contentsOf: imageUrl),
-         let image = UIImage(data: data) {
-        DispatchQueue.main.async {
-          self.uiImage = image
-          self.isLoading = false
-        }
-      } else {
-        DispatchQueue.main.async {
-          self.isLoading = false
-        }
-      }
-    }
-  }
 }
 
-struct FavoriteMovieCardView_Previews: PreviewProvider {
-  static var previews: some View {    
+struct MiniMovieCardButton_Previews: PreviewProvider {
+  static var previews: some View {
     let sampleMovie = SampleData.favoriteMovie
     
     MiniMovieCardButton(navStack:.constant([]),
